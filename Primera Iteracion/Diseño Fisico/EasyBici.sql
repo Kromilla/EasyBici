ab56c9aebfdaa8b7132d6939171851c4b4c9ad55
kromilla <carlos15.ci15@gmail.com>
2025-10-11 22:01:54 -0500

    Eliminar conflicto de fusión en la tabla Departamentos en EasyBici.sql

--- Primera Iteracion/Diseño Fisico/EasyBici.sql
+++ Primera Iteracion/Diseño Fisico/EasyBici.sql
@@ -166,8 +166,5 @@ CREATE TABLE Departamentos (
     --Validaciones
     CONSTRAINT PK_Departamentos PRIMARY KEY (id_departamento),
     CONSTRAINT UQ_Departamentos_Nombre UNIQUE (nombre)
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> c6d0d203e7f0f5d4c1c75260168bcd252c156c63
+
