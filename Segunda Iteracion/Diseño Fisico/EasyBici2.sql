237ec161bd60727f952bda50ca1a017ab78b2155
kromilla <carlos15.ci15@gmail.com>
2025-10-12 22:32:23 -0500

    Agregar tablas para el modelo de datos en EasyBici2.sql

--- Segunda Iteracion/Diseño Fisico/EasyBici2.sql
+++ Segunda Iteracion/Diseño Fisico/EasyBici2.sql
@@ -1,34 +1,38 @@
 -- Tabla Modelo
+
 CREATE TABLE Modelos (
     id_modelo INT IDENTITY(1,1),
     nombre VARCHAR(100) NOT NULL,
-    año DATETIME NOT NULL,
+    año VARCHAR(100) NOT NULL,
     id_marca INT NOT NULL,
-    
+
     -- validaciones
     CONSTRAINT PK_Modelos PRIMARY KEY (id_modelo),
     CONSTRAINT UQ_Modelos_Nombres UNIQUE (nombre),
     CONSTRAINT FK_Modelos_Marcas FOREIGN KEY (id_marca) REFERENCES Marcas(id_marca)
-
 );
 
+
 -- Tabla Bicicleta
-CREATE TABLE Bicicletas (
+  CREATE TABLE Bicicletas (
     id_bicicleta INT IDENTITY(1,1),
-    estado VARCHAR(100) NOT NULL,
     tamaño_marco VARCHAR(100) NOT NULL,
+    color VARCHAR(100) NOT NULL,
+    id_estado INT NOT NULL,
     id_modelo INT NOT NULL,
     id_punto_alquiler INT NOT NULL,
     id_tipo_bicicleta INT NOT NULL,
     id_asistencia INT NOT NULL,
 
-    --Validaciones
+    -- Validaciones
     CONSTRAINT PK_Bicicletas PRIMARY KEY (id_bicicleta),
     CONSTRAINT FK_Bicicletas_Modelos FOREIGN KEY (id_modelo) REFERENCES Modelos(id_modelo),
     CONSTRAINT FK_Bicicletas_Puntos_Alquiler FOREIGN KEY (id_punto_alquiler) REFERENCES Puntos_De_Alquiler(id_punto_alquiler),
     CONSTRAINT FK_Bicicletas_Tipos_Bicicletas FOREIGN KEY (id_tipo_bicicleta) REFERENCES Tipos_De_Bicicletas(id_tipo_bicicleta),
-    CONSTRAINT FK_Bicicletas_Asistencias FOREIGN KEY (id_asistencia) REFERENCES Asistencias(id_asistencia)
+    CONSTRAINT FK_Bicicletas_Asistencias FOREIGN KEY (id_asistencia) REFERENCES Asistencias(id_asistencia),
+    CONSTRAINT FK_Bicicletas_Estados FOREIGN KEY (id_estado) REFERENCES Estados(id_estado)
 );
+
 
 -- Tabla Tipo De Bicicleta
     CREATE TABLE Tipos_De_Bicicletas (
@@ -53,30 +57,26 @@ CREATE TABLE Bicicletas (
 );
 
 -- Tabla Marca
-  CREATE TABLE Marcas (
+    CREATE TABLE Marcas (
     id_marca INT IDENTITY(1,1),
     nombre VARCHAR(100) NOT NULL,
-    pais_origen VARCHAR(100) NOT NULL,
-    sitio_web VARCHAR(100) NOT NULL,
+    sitio_web VARCHAR(100) NOT NULL
 
     --Validaciones
     CONSTRAINT PK_Marcas PRIMARY KEY (id_marca),
     CONSTRAINT UQ_Marcas_Nombre UNIQUE (nombre),
     CONSTRAINT UQ_Marcas_Sitio_Web UNIQUE (sitio_web)
 );
-
--- Tabla Bicicletas Facturadas
-CREATE TABLE Bicicletas_Facturadas (
-    id_bicicleta_facturada INT IDENTITY(1,1),
-    id_bicicleta INT NOT NULL,
-    id_factura INT NOT NULL,
+-- Tabla Metodo De Pago
+ CREATE TABLE Metodos_Pago (
+    id_metodo_pago INT IDENTITY(1,1),
+    nombre VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(100) NULL,
 
     --Validaciones 
-    CONSTRAINT PK_Bicicletas_Facturadas PRIMARY KEY (id_bicicleta_facturada),
-    CONSTRAINT FK_Bicicletas_Facturadas_Bicicletas FOREIGN KEY (id_bicicleta) REFERENCES Bicicleta(id_bicicleta),
-    CONSTRAINT FK_Bicicletas_Facturadas_Facturas FOREIGN KEY (id_factura) REFERENCES Factura(id_factura)
+    CONSTRAINT PK_Metodos_Pago PRIMARY KEY (id_metodo_pago),
+    CONSTRAINT UQ_Metodos_Pago_Nombre UNIQUE (nombre)
 );
-
 -- Tabla Factura 
    CREATE TABLE Facturas (
     id_factura INT IDENTITY(1,1),
@@ -88,16 +88,16 @@ CREATE TABLE Bicicletas_Facturadas (
     CONSTRAINT PK_Facturas PRIMARY KEY (id_factura),
     CONSTRAINT FK_Facturas_Metodos_Pago FOREIGN KEY (id_metodo_pago) REFERENCES Metodos_Pago(id_metodo_pago)
 );
-
--- Tabla Metodo De Pago
- CREATE TABLE Metodos_Pago (
-    id_metodo_pago INT IDENTITY(1,1),
-    nombre VARCHAR(100) NOT NULL,
-    descripcion VARCHAR(100) NULL,
+-- Tabla Bicicletas Facturadas
+CREATE TABLE Bicicletas_Facturadas (
+    id_bicicleta_facturada INT IDENTITY(1,1),
+    id_bicicleta INT NOT NULL,
+    id_factura INT NOT NULL,
 
     --Validaciones 
-    CONSTRAINT PK_Metodos_Pago PRIMARY KEY (id_metodo_pago),
-    CONSTRAINT UQ_Metodos_Pago_Nombre UNIQUE (nombre)
+    CONSTRAINT PK_Bicicletas_Facturadas PRIMARY KEY (id_bicicleta_facturada),
+    CONSTRAINT FK_Bicicletas_Facturadas_Bicicletas FOREIGN KEY (id_bicicleta) REFERENCES Bicicletas(id_bicicleta),
+    CONSTRAINT FK_Bicicletas_Facturadas_Facturas FOREIGN KEY (id_factura) REFERENCES Facturas(id_factura)
 );
 
 -- Tabla Plan Alquiler
@@ -109,9 +109,9 @@ CREATE TABLE Planes_Alquiler (
     fecha_inicio_vigencia DATETIME NOT NULL,
     fecha_fin_vigencia DATETIME NOT NULL,
     duracion VARCHAR(100) NOT NULL,
-    decripcion VARCHAR(100) NULL,
+    descripcion VARCHAR(100) NULL,
 
-    --Validaciones 
+    -- Validaciones 
     CONSTRAINT PK_Planes_Alquiler PRIMARY KEY (id_plan_alquiler),
     CONSTRAINT UQ_Planes_Alquiler UNIQUE (nombre)
 );
@@ -121,45 +121,15 @@ CREATE TABLE Planes_Alquiler (
 CREATE TABLE Tarifas (
     id_tarifa INT IDENTITY(1,1),
     id_tipo_bicicleta INT NOT NULL,
-    id_plan_alquiler INT NOT NULL,
+    tarifa_plan INT NOT NULL,
     valor FLOAT NOT NULL,
 
-    --Validaciones
-    CONSTRAINT PK_Tarifas PRIMARY KEY (id_tarifa)
-    CONSTRAINT FK_Tarifas_Tipos_Bicicletas FOREIGN KEY (id_tipo_bicicleta) REFERENCES Tipos_De_Bicicleta(id_tipo_bicicleta),
-    CONSTRAINT FK_Tarifas_Planes_Alquiler FOREIGN KEY (id_plan_alquiler) REFERENCES Planes_Alquiler(id_plan_alquiler)
+    -- Validaciones
+    CONSTRAINT PK_Tarifas PRIMARY KEY (id_tarifa),
+    CONSTRAINT FK_Tarifas_Tipos_Bicicletas FOREIGN KEY (id_tipo_bicicleta) REFERENCES Tipos_De_Bicicletas(id_tipo_bicicleta),
+    CONSTRAINT FK_Tarifas_Planes_Alquiler FOREIGN KEY (tarifa_plan) REFERENCES Planes_Alquiler(id_plan_alquiler)
 );
 
--- Tabla Punto De Alquiler
-CREATE TABLE Puntos_De_Alquiler (
-    id_punto_alquiler INT IDENTITY(1,1),
-    nombre VARCHAR(100) NOT NULL,
-    correo VARCHAR(100) NOT NULL,
-    capacidad INT NOT NULL,
-    direccion VARCHAR(100) NOT NULL,
-    horario VARCHAR(100) NOT NULL,
-    id_ciudad INT NOT NULL,
-
-    --Validaciones
-    CONSTRAINT PK_Puntos_Alquiler PRIMARY KEY (id_punto_alquiler),
-    CONSTRAINT UQ_Puntos_Alquiler_nombre UNIQUE (nombre),
-    CONSTRAINT UQ_Puntos_Alquiler_Correo UNIQUE (correo),
-    CONSTRAINT UQ_Puntos_Alquiler_direccion UNIQUE (direccion),
-    CONSTRAINT FK_Puntos_Alquiler_Ciudades FOREIGN KEY (id_ciudad) REFERENCES Ciudades(id_ciudad)
-);
-
-
--- Tabla Cuidad
-CREATE TABLE Ciudades (
-    id_ciudad INT IDENTITY(1,1),
-    nombre VARCHAR(100) NOT NULL,
-    id_departamento INT NOT NULL,
-
-    --Validaciones
-    CONSTRAINT PK_Ciudades PRIMARY KEY (id_ciudad),
-    CONSTRAINT UQ_Ciudades_Nombre UNIQUE (nombre),
-    CONSTRAINT FK_Ciudades_Departamentos FOREIGN KEY (id_departamento) REFERENCES Departamentos(id_departamento)
-);
 
 -- Tabla Departamento
 CREATE TABLE Departamentos (
@@ -171,6 +141,39 @@ CREATE TABLE Departamentos (
     CONSTRAINT UQ_Departamentos_Nombre UNIQUE (nombre)
 );
 
+-- Tabla Cuidad
+CREATE TABLE Ciudades (
+    id_ciudad INT IDENTITY(1,1),
+    nombre VARCHAR(100) NOT NULL,
+    latitud VARCHAR(100),
+    longitud VARCHAR(100),
+    id_departamento INT NOT NULL,
+
+    -- Validaciones
+    CONSTRAINT PK_Ciudades PRIMARY KEY (id_ciudad),
+    CONSTRAINT UQ_Ciudades_Nombre UNIQUE (nombre),
+    CONSTRAINT FK_Ciudades_Departamentos FOREIGN KEY (id_departamento) REFERENCES Departamentos(id_departamento)
+);
+
+
+-- Tabla Punto De Alquiler
+CREATE TABLE Puntos_De_Alquiler (
+    id_punto_alquiler INT IDENTITY(1,1),
+    nombre VARCHAR(100) NOT NULL,
+    telefono VARCHAR(100) NOT NULL,
+    capacidad INT NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    id_horario INT NOT NULL,
+    id_ciudad INT NOT NULL,
+
+    -- Validaciones
+    CONSTRAINT PK_Puntos_Alquiler PRIMARY KEY (id_punto_alquiler),
+    CONSTRAINT UQ_Puntos_Alquiler_nombre UNIQUE (nombre),
+    CONSTRAINT UQ_Puntos_Alquiler_direccion UNIQUE (direccion),
+    CONSTRAINT FK_Puntos_Alquiler_Ciudades FOREIGN KEY (id_ciudad) REFERENCES Ciudades(id_ciudad),
+    CONSTRAINT FK_Puntos_Alquiler_Horarios FOREIGN KEY (id_horario) REFERENCES Horarios_Punto_Alquiler(id_horario)
+);
+
 -- Tabla Estados
 CREATE TABLE Estados (
     id_estado INT IDENTITY(1,1),
@@ -179,19 +182,21 @@ CREATE TABLE Estados (
 );
 
 -- Tabla Horarios_Punto_Alquiler
+
 CREATE TABLE Horarios_Punto_Alquiler (
-    id INT IDENTITY(1,1),
-    dia_semana NVARCHAR(20) NOT NULL,
-    hora_inicio TIME NOT NULL,
-    hora_fin TIME NOT NULL
+    id_horario INT IDENTITY(1,1),
+    dia_de_semana NVARCHAR(20) NOT NULL,
+    hora_inicio DATETIME NOT NULL,
+    hora_fin DATETIME NOT NULL
 );
 
 -- Tabla Tipos_Documentos_De_Identificacion
 CREATE TABLE Tipos_Documentos_De_Identificacion (
-    id INT IDENTITY(1,1),
+    id_tipo_documento INT IDENTITY(1,1),
     nombre NVARCHAR(100) NOT NULL,
     descripcion NVARCHAR(100) NULL
 );
+
 
 -- Tabla Personas
 CREATE TABLE Personas (
@@ -212,24 +217,29 @@ CREATE TABLE Cargos (
     descripcion VARCHAR(250) NULL
 );
 
+
 -- Tabla Trabajadores
 CREATE TABLE Trabajadores (
-    id INT IDENTITY(1,1),
-    numero_trabajador VARCHAR(100) NOT NULL,
-    id_cargo INT NOT NULL
+    id_trabajador INT IDENTITY(1,1),
+    id_cargo INT NOT NULL,
+    id_persona INT NOT NULL
 );
 
+
 -- Tabla Horarios_Empleado
-CREATE TABLE Horarios_Empleado (
-    id INT IDENTITY(1,1),
+
+CREATE TABLE Horarios_Empleados (
+    id_horario_empleado INT IDENTITY(1,1),
+    id_trabajador INT NOT NULL,
+    hora_inicio DATETIME NOT NULL,
     dia_semana VARCHAR(10) NOT NULL,
-    hora_inicio TIME NOT NULL,
-    hora_fin TIME NOT NULL
+    hora_fin DATETIME NOT NULL
 );
 
 -- Tabla Usuarios
 CREATE TABLE Usuarios (
-    id INT IDENTITY(1,1),
-    metodo_pago_preferido INT NULL
+    id_usuario INT IDENTITY(1,1),
+    id_persona INT NOT NULL,
+    id_metodo_de_pago_preferido INT NULL
 );
 
